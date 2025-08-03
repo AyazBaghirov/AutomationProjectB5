a62634e794181372ab2aee119e2ad5288e0ade07
Admin <ayazbagirov89@gmail.com>
2025-08-03 14:38:31 -0400

    Initial commit

---
+++ src/test/java/io/loop/test/day4/T3_css_practice.java
@@ -0,0 +1,28 @@
+package io.loop.test.day4;
+
+public class T3_css_practice {
+    public static void main(String[] args) {
+
+
+    /*
+    go to docuport app
+    identify docuport with css
+    get value of the attribute
+    validate if it is "Docuport"
+
+    the rest is home work
+    locate:
+    username
+    password
+    login
+        with css
+    login to docuport
+    locate home icon of docuport after login or any other element after login
+    validated that you logged in
+
+     */
+
+
+
+    }
+}
